62cd7ba99e7c76d5a9d5cffb09d317fb19c5895c
Varun7691 <varun7691@gmail.com>
2015-07-10 11:25:17 +0530

    Adding toast message on TextView click

--- app/src/main/java/com/vfsglobal/wolf/androidstudiogithubsample/MainActivity.java
+++ app/src/main/java/com/vfsglobal/wolf/androidstudiogithubsample/MainActivity.java
@@ -4,13 +4,28 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Sample.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     @Override
